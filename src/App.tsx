4b82d25ca4f988ca866e34a874bd8bc6090d169c
Admin <admin@MacBook-Pro-de-Admin.local>
2025-05-26 16:35:45 -0600

    Adding delete todo functionality and login authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const { signOut } = useAuthenticator();
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -13,8 +15,12 @@ function App() {
     });
   }, []);
 
-  function createTodo() {
+  function createTodo(): void {
     client.models.Todo.create({ content: window.prompt("Todo content") });
+  }
+
+  function deleteTodo(id: string): void {
+    client.models.Todo.delete({id})
   }
 
   return (
@@ -23,7 +29,11 @@ function App() {
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li 
+            onClick={() => deleteTodo(todo.id)} 
+            key={todo.id}>
+              {todo.content}
+          </li>
         ))}
       </ul>
       <div>
@@ -33,6 +43,8 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <br/>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
